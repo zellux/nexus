09281ae7457a8e482bccad43341cdc60a2eaff79
Aleksey Pesterev <alekseyp@mit.edu>
2008-09-18 20:39:45 -0400

    added lab3 code

--- grade.sh
+++ grade.sh
@@ -52,34 +52,219 @@ runbochs () {
 }
 
 
+# Usage: runtest <tagname> <defs> <strings...>
+runtest () {
+	perl -e "print '$1: '"
+	rm -f obj/kern/init.o obj/kern/kernel obj/kern/bochs.img 
+	[ "$preservefs" = y ] || rm -f obj/fs/fs.img
+	if $verbose
+	then
+		echo "gmake $2... "
+	fi
+	gmake $2 >$out
+	if [ $? -ne 0 ]
+	then
+		echo gmake $2 failed 
+		exit 1
+	fi
+	runbochs
+	if [ ! -s bochs.out ]
+	then
+		echo 'no bochs.out'
+	else
+		shift
+		shift
+		continuetest "$@"
+	fi
+}
 
-gmake
-runbochs
+quicktest () {
+	perl -e "print '$1: '"
+	shift
+	continuetest "$@"
+}
+
+stubtest () {
+    perl -e "print qq|$1: OK $2\n|";
+    shift
+    score=`expr $pts + $score`
+}
+
+continuetest () {
+	okay=yes
+
+	not=false
+	for i
+	do
+		if [ "x$i" = "x!" ]
+		then
+			not=true
+		elif $not
+		then
+			if egrep "^$i\$" bochs.out >/dev/null
+			then
+				echo "got unexpected line '$i'"
+				if $verbose
+				then
+					exit 1
+				fi
+				okay=no
+			fi
+			not=false
+		else
+			egrep "^$i\$" bochs.out >/dev/null
+			if [ $? -ne 0 ]
+			then
+				echo "missing '$i'"
+				if $verbose
+				then
+					exit 1
+				fi
+				okay=no
+			fi
+			not=false
+		fi
+	done
+	if [ "$okay" = "yes" ]
+	then
+		score=`expr $pts + $score`
+		echo OK $time
+	else
+		echo WRONG $time
+	fi
+}
+
+# Usage: runtest1 [-tag <tagname>] <progname> [-Ddef...] STRINGS...
+runtest1 () {
+	if [ $1 = -tag ]
+	then
+		shift
+		tag=$1
+		prog=$2
+		shift
+		shift
+	else
+		tag=$1
+		prog=$1
+		shift
+	fi
+	runtest1_defs=
+	while expr "x$1" : 'x-D.*' >/dev/null; do
+		runtest1_defs="DEFS+='$1' $runtest1_defs"
+		shift
+	done
+	runtest "$tag" "DEFS='-DTEST=_binary_obj_user_${prog}_start' DEFS+='-DTESTSIZE=_binary_obj_user_${prog}_size' $runtest1_defs" "$@"
+}
+
+
 
 score=0
 
-echo_n "Page directory: "
- if grep "check_boot_pgdir() succeeded!" bochs.out >/dev/null
- then
-	score=`expr 20 + $score`
-	echo OK $time
- else
-	echo WRONG $time
- fi
+runtest1 hello \
+	'.00000000. new env 00001000' \
+	'hello, world' \
+	'i am environment 00001000' \
+	'.00001000. exiting gracefully' \
+	'.00001000. free env 00001000' \
+	'Destroyed the only environment - nothing more to do!'
 
-echo_n "Page management: "
- if grep "page_check() succeeded!" bochs.out >/dev/null
- then
-	score=`expr 30 + $score`
-	echo OK $time
- else
-	echo WRONG $time
- fi
+# the [00001000] tags should have [] in them, but that's 
+# a regular expression reserved character, and i'll be damned if
+# I can figure out how many \ i need to add to get through 
+# however many times the shell interprets this string.  sigh.
 
-echo "Score: $score/50"
+runtest1 buggyhello \
+	'.00001000. user_mem_check assertion failure for va 00000001' \
+	'.00001000. free env 00001000'
 
-if [ $score -lt 50 ]; then
+runtest1 evilhello \
+	'.00001000. user_mem_check assertion failure for va f0100...' \
+	'.00001000. free env 00001000'
+
+runtest1 divzero \
+	! '1/0 is ........!' \
+	'Incoming TRAP frame at 0xefbfff..' \
+        'TRAP frame at 0xf.......' \
+	'  trap 0x00000000 Divide error' \
+	'  eip  0x008.....' \
+	'  ss   0x----0023' \
+	'.00001000. free env 00001000'
+
+runtest1 breakpoint \
+	'Welcome to the JOS kernel monitor!' \
+	'Incoming TRAP frame at 0xefbfffbc' \
+        'TRAP frame at 0xf.......' \
+	'  trap 0x00000003 Breakpoint' \
+	'  eip  0x008.....' \
+	'  ss   0x----0023' \
+	! '.00001000. free env 00001000'
+
+runtest1 softint \
+	'Welcome to the JOS kernel monitor!' \
+	'Incoming TRAP frame at 0xefbfffbc' \
+        'TRAP frame at 0xf.......' \
+	'  trap 0x0000000d General Protection' \
+	'  eip  0x008.....' \
+	'  ss   0x----0023' \
+	'.00001000. free env 00001000'
+
+runtest1 badsegment \
+	'Incoming TRAP frame at 0xefbfffbc' \
+        'TRAP frame at 0xf.......' \
+	'  trap 0x0000000d General Protection' \
+	'  err  0x00000028' \
+	'  eip  0x008.....' \
+	'  ss   0x----0023' \
+	'.00001000. free env 00001000'
+
+
+runtest1 faultread \
+	! 'I read ........ from location 0!' \
+	'.00001000. user fault va 00000000 ip 008.....' \
+	'Incoming TRAP frame at 0xefbfffbc' \
+        'TRAP frame at 0xf.......' \
+	'  trap 0x0000000e Page Fault' \
+	'  err  0x00000004' \
+	'.00001000. free env 00001000'
+
+runtest1 faultreadkernel \
+	! 'I read ........ from location 0xf0100000!' \
+	'.00001000. user fault va f0100000 ip 008.....' \
+	'Incoming TRAP frame at 0xefbfffbc' \
+        'TRAP frame at 0xf.......' \
+	'  trap 0x0000000e Page Fault' \
+	'  err  0x00000005' \
+	'.00001000. free env 00001000' \
+
+runtest1 faultwrite \
+	'.00001000. user fault va 00000000 ip 008.....' \
+	'Incoming TRAP frame at 0xefbfffbc' \
+        'TRAP frame at 0xf.......' \
+	'  trap 0x0000000e Page Fault' \
+	'  err  0x00000006' \
+	'.00001000. free env 00001000'
+
+runtest1 faultwritekernel \
+	'.00001000. user fault va f0100000 ip 008.....' \
+	'Incoming TRAP frame at 0xefbfffbc' \
+        'TRAP frame at 0xf.......' \
+	'  trap 0x0000000e Page Fault' \
+	'  err  0x00000007' \
+	'.00001000. free env 00001000'
+
+runtest1 testbss \
+	'Making sure bss works right...' \
+	'Yes, good.  Now doing a wild write off the end...' \
+	'.00001000. user fault va 00c..... ip 008.....' \
+	'.00001000. free env 00001000'
+
+
+
+echo "Score: $score/60"
+
+if [ $score -lt 60 ]; then
     exit 1
 fi
+
 
 
