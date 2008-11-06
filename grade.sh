6bd4f5821ff7b7b3fbbd364b8da7e508953daeb3
Aleksey Pesterev <alekseyp@mit.edu>
2008-11-06 18:10:59 -0500

    remove lab5 test code

--- grade.sh
+++ grade.sh
@@ -22,140 +22,6 @@ echo_n () {
 	awk 'BEGIN { printf("'"$*"'"); }' </dev/null
 }
 
-runbochs () {
-	# Find the address of the kernel readline function,
-	# which the kernel monitor uses to read commands interactively.
-	brkaddr=`grep 'readline$' obj/kern/kernel.sym | sed -e's/ .*$//g'`
-	#echo "brkaddr $brkaddr"
-
-	# Run Bochs, setting a breakpoint at readline(),
-	# and feeding in appropriate commands to run, then quit.
-	t0=`date +%s.%N 2>/dev/null`
-	(
-		# The sleeps are necessary in some Bochs to 
-		# make it parse each line separately.  Sleeping 
-		# here sure beats waiting for the timeout.
-		echo vbreak 0x8:0x$brkaddr
-		sleep .5
-		echo c
-		# EOF will do just fine to quit.
-	) | (
-		ulimit -t $timeout
-		# date
-		bochs -q 'display_library: nogui' \
-			'parport1: enabled=1, file="bochs.out"' 
-		# date
-	) >$out 2>$err
-	t1=`date +%s.%N 2>/dev/null`
-	time=`echo "scale=1; ($t1-$t0)/1" | sed 's/.N/.0/g' | bc 2>/dev/null`
-	time="(${time}s)"
-}
-
-
-# Usage: runtest <tagname> <defs> <strings...>
-runtest () {
-	perl -e "print '$1: '"
-	rm -f obj/kern/init.o obj/kern/kernel obj/kern/bochs.img 
-	[ "$preservefs" = y ] || rm -f obj/fs/fs.img
-	if $verbose
-	then
-		echo "gmake $2... "
-	fi
-	gmake $2 >$out
-	if [ $? -ne 0 ]
-	then
-		echo gmake $2 failed 
-		exit 1
-	fi
-	runbochs
-	if [ ! -s bochs.out ]
-	then
-		echo 'no bochs.out'
-	else
-		shift
-		shift
-		continuetest "$@"
-	fi
-}
-
-quicktest () {
-	perl -e "print '$1: '"
-	shift
-	continuetest "$@"
-}
-
-stubtest () {
-    perl -e "print qq|$1: OK $2\n|";
-    shift
-    score=`expr $pts + $score`
-}
-
-continuetest () {
-	okay=yes
-
-	not=false
-	for i
-	do
-		if [ "x$i" = "x!" ]
-		then
-			not=true
-		elif $not
-		then
-			if egrep "^$i\$" bochs.out >/dev/null
-			then
-				echo "got unexpected line '$i'"
-				if $verbose
-				then
-					exit 1
-				fi
-				okay=no
-			fi
-			not=false
-		else
-			egrep "^$i\$" bochs.out >/dev/null
-			if [ $? -ne 0 ]
-			then
-				echo "missing '$i'"
-				if $verbose
-				then
-					exit 1
-				fi
-				okay=no
-			fi
-			not=false
-		fi
-	done
-	if [ "$okay" = "yes" ]
-	then
-		score=`expr $pts + $score`
-		echo OK $time
-	else
-		echo WRONG $time
-	fi
-}
-
-# Usage: runtest1 [-tag <tagname>] <progname> [-Ddef...] STRINGS...
-runtest1 () {
-	if [ $1 = -tag ]
-	then
-		shift
-		tag=$1
-		prog=$2
-		shift
-		shift
-	else
-		tag=$1
-		prog=$1
-		shift
-	fi
-	runtest1_defs=
-	while expr "x$1" : 'x-D.*' >/dev/null; do
-		runtest1_defs="DEFS+='$1' $runtest1_defs"
-		shift
-	done
-	runtest "$tag" "DEFS='-DTEST=_binary_obj_user_${prog}_start' DEFS+='-DTESTSIZE=_binary_obj_user_${prog}_size' $runtest1_defs" "$@"
-}
-
 qemu_test_httpd() {
 	pts=5
 
@@ -286,92 +152,18 @@ resetfs() {
 	gmake obj/fs/fs.img >$out
 }
 
-
-resetfs
-
-runtest1 -tag 'fs i/o [testfsipc]' testfsipc \
-	'FS can do I/O' \
-	! 'idle loop can do I/O' \
-
-quicktest 'read_block [testfsipc]' \
-	'superblock is good' \
-
-quicktest 'write_block [testfsipc]' \
-	'write_block is good' \
-
-quicktest 'read_bitmap [testfsipc]' \
-	'read_bitmap is good' \
-
-quicktest 'alloc_block [testfsipc]' \
-	'alloc_block is good' \
-
-quicktest 'file_open [testfsipc]' \
-	'file_open is good' \
-
-quicktest 'file_get_block [testfsipc]' \
-	'file_get_block is good' \
-
-quicktest 'file_truncate [testfsipc]' \
-	'file_truncate is good' \
-
-quicktest 'file_flush [testfsipc]' \
-	'file_flush is good' \
-
-quicktest 'file rewrite [testfsipc]' \
-	'file rewrite is good' \
-
-quicktest 'serv_* [testfsipc]' \
-	'serve_open is good' \
-	'serve_map is good' \
-	'serve_close is good' \
-	'stale fileid is good' \
-
-pts=5
-runtest1 -tag 'motd display [writemotd]' writemotd \
-	'OLD MOTD' \
-	'This is /motd, the message of the day.' \
-	'NEW MOTD' \
-	'This is the NEW message of the day!' \
-
-preservefs=y
-runtest1 -tag 'motd change [writemotd]' writemotd \
-	'OLD MOTD' \
-	'This is the NEW message of the day!' \
-	'NEW MOTD' \
-	! 'This is /motd, the message of the day.' \
-
-pts=8
-preservefs=n
-runtest1 -tag 'spawn via icode [icode]' icode \
-	'icode: read /motd' \
-	'This is /motd, the message of the day.' \
-	'icode: spawn /init' \
-	'init: running' \
-	'init: data seems okay' \
-	'icode: exiting' \
-	'init: bss seems okay' \
-	"init: args: 'init' 'initarg1' 'initarg2'" \
-	'init: exiting' \
-
-echo PART A SCORE: $score/40
-
-partascore=$score
-
-
 score=0
-pts=45
-preservefs=y
+pts=85
+preservefs=n
 runtestq -tag 'tcp echo server [tcpsrv]' tcpsrv
 
 #pts=15 # points are allocated in the test code
-preservefs=y
+preservefs=n
 runtestq -tag 'web server [httpd]' httpd
 
-echo PART B SCORE: $score/60
+echo PART A SCORE: $score/100
 
-partbscore=$score
-
-if [ $partascore -lt 40 -o $partbscore -lt 60 ]; then
+if [ $score -lt 100 ]; then
     exit 1
 fi
 
