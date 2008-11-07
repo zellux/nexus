798ccdb42f760dbbee17148b9b4a8df917e09dcb
Aleksey Pesterev <alekseyp@mit.edu>
2008-11-07 14:36:59 -0500

    helps with the terminal lock up

--- qemu.sh
+++ qemu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-qemu -hda obj/kern/bochs.img -hdb obj/fs/fs.img -parallel /dev/stdout \
+qemu -hda obj/kern/bochs.img -hdb obj/fs/fs.img -parallel stdio \
      -net user -net nic,model=i82559er,macaddr=52:54:00:12:34:56 \
      -redir tcp:8080::80 -redir tcp:4242::10000 "$@"
