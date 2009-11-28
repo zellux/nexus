1382f4ee6b224cd11408e8dfad8a4b33e2ec9555
Wang Yuanxuan <zellux@gmail.com>
2009-11-28 09:55:33 +0800

    Pass faultallocbad, fix a bug in Lab 3

--- kern/pmap.c
+++ kern/pmap.c
@@ -759,6 +759,7 @@ user_mem_check(struct Env *env, const void *va, size_t len, int perm)
     unsigned end = ROUNDUP(vaddr + len, PGSIZE);
     pte_t *pte;
     unsigned flags;
+    struct Page *pp;
     
     /* if (env->env_tf.tf_cs == GD_KT) { */
     /*     return 0; */
@@ -770,9 +771,9 @@ user_mem_check(struct Env *env, const void *va, size_t len, int perm)
     }
         
     while (start < end) {
-        page_lookup(env->env_pgdir, (void *) start, &pte);
+        pp = page_lookup(env->env_pgdir, (void *) start, &pte);
         flags = (*pte) & 0xFFF;
-        if ((perm & (~flags)) > 0) {
+        if (!pp || (perm & (~flags)) > 0) {
             user_mem_check_addr = vaddr;
             return -E_FAULT;
         }
